a9aef32078feb99705057b9da65554d2f39c379e
stone.al@husky.neu.edu <stone.al@husky.neu.edu>
2018-02-08 13:35:36 -0500

    Short Read Alignment

---
+++ RNA-Seq/alignAll.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Initialize variable to contain the directory of trimmed paired fastq files 
+fastqPath="/home/stone.al/workspace/BINF6309/RNA-Seq/Paired"
+#initialize variable to contain the suffix for the left reads
+leftSuffix=".R1.fastq"
+rightSuffix=".R2.fastq"
+pairedOutPath="sam/"
+#Loop through all the left-read fastq files in $fastqPath
+for leftInFile in $fastqPath*$leftSuffix
+do
+	#Remove the path and left-read suffix from the filename and assign to sampleName
+        pathRemoved="${leftInFile/$fastqPath}"
+        sampleName="${pathRemoved/$leftSuffix/}"
+	echo nice -n 19 gsnap \
+	-A sam \
+	-s AiptasiaGmapIIT.iit \
+	-D . \
+	-d AiptasiaGmapDb \
+	$fastqPath$sampleName$leftSuffix \
+        $fastqPath$sampleName$rightSuffix \
+        $pairedOutPath$sampleName$leftSuffix \
+        $pairedOutPath$sampleName$rightSuffix \
+	#1>Aip02.sam 2>Aip02.err 
+done
